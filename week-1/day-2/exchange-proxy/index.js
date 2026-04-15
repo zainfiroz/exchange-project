422b8f8552cb4c8db71980012961a6dd34382afe
Zain-firoz <zainfiroz007@gmail.com>
2026-04-15 19:07:32 +0530

    fix port for railway

--- week-1/day-2/exchange-proxy/index.js
+++ week-1/day-2/exchange-proxy/index.js
@@ -26,7 +26,7 @@ app.use('/', createProxyMiddleware({
     }
 }));
 
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Proxy server running on http://localhost:${port}`);
 });
